2f932ae07a0ce08a914eff3cbd0f3423acb76e11
hobby <g99949@hotmail.com>
2021-11-29 23:23:57 +0800

    [Feature] 刪除產品

---
+++ src/components/DeleteModal.vue
@@ -0,0 +1,76 @@
+<template>
+  <div
+    ref="modal"
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-xl" role="document">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-dark text-white">
+          <h5 class="modal-title" id="exampleModalLabel">
+            <span>刪除{{tempProduct.title}}</span>
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close">
+          </button>
+        </div>
+        <div class="modal-body">
+          是否刪除 <span class="text-danger">{{tempProduct.title}}</span>（刪除後將無法恢復）。
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-outline-secondary"
+            data-bs-dismiss="modal"
+          >
+            取消
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            @click="$emit('delete-product', tempProduct.id)"
+          >
+            確認刪除
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import modalMixin from '@/mixins/modalMixin'
+export default {
+  name: 'ProductModal',
+  props: {
+    product: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+  mixins: [modalMixin],
+  data () {
+    return {
+      modal: {},
+      tempProduct: {}
+    }
+  },
+  watch: {
+    product (newVal) {
+      this.tempProduct = this.product
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -3,7 +3,7 @@
     <button
       class="btn btn-primary"
       type="button"
-      @click="$refs.productModal.showModal()"
+      @click="openModal(true)"
     >
       新增
     </button>
@@ -20,7 +20,10 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="item in products" :key="item.id">
+      <tr
+        v-for="item in products"
+        :key="item.id"
+      >
         <td>{{ item.category }}</td>
         <td>{{ item.title }}</td>
         <td class="text-right">
@@ -35,39 +38,105 @@
         </td>
         <td>
           <div class="btn-group">
-            <button class="btn btn-outline-primary btn-sm">編輯</button>
-            <button class="btn btn-outline-danger btn-sm">刪除</button>
+            <button
+              class="btn btn-outline-primary btn-sm"
+              @click="openModal(false, item)"
+            >
+              編輯
+            </button>
+            <button
+              class="btn btn-outline-danger btn-sm"
+              @click="openDeleteModal(item)"
+            >
+              刪除
+            </button>
           </div>
         </td>
       </tr>
     </tbody>
   </table>
-  <ProductModal ref="productModal" />
+  <ProductModal ref="productModal" :product="tempProduct" @update-product="updateProducts" />
+  <DeleteModal ref="deleteModal" :product="tempProduct" @delete-product="deleteProduct" />
 </template>
 
 <script>
 import ProductModal from '../components/ProductModal.vue'
+import DeleteModal from '../components/DeleteModal.vue'
 export default {
   name: 'Products',
-  components: { ProductModal },
+  components: { ProductModal, DeleteModal },
   data () {
     return {
       products: [],
-      pagination: {}
+      pagination: {},
+      tempProduct: {},
+      isNew: false
     }
   },
   created () {
     this.getProducts()
   },
   methods: {
+    // 開啟新增/編輯燈箱
+    openModal (isNew, item) {
+      this.tempProduct = isNew
+        ? {}
+        : { ...item }
+
+      this.isNew = isNew
+      const productComponent = this.$refs.productModal
+      productComponent.showModal()
+    },
+    // 開啟刪除燈箱
+    openDeleteModal (item) {
+      this.tempProduct = { ...item }
+      const productComponent = this.$refs.deleteModal
+      productComponent.showModal()
+    },
+    // 取得產品列表
     getProducts () {
       const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products`
       this.$http.get(api)
         .then((res) => {
-          console.log(res)
           if (res.data.success) {
             this.products = res.data.products
             this.pagination = res.data.pagination
+          }
+        })
+    },
+    // 更新產品
+    updateProducts (item) {
+      const productComponent = this.$refs.productModal
+
+      // 若是新增狀態
+      let api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product`
+      let httpMethod = 'post'
+
+      // 若是編輯狀態
+      if (!this.isNew) {
+        api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product/${item.id}`
+        httpMethod = 'put'
+      }
+      // 打 API
+      this.$http[httpMethod](api, { data: item })
+        .then((res) => {
+          if (res.data.success) {
+            productComponent.hideModal()
+            this.getProducts()
+          }
+        })
+    },
+    // 刪除產品
+    deleteProduct (id) {
+      console.log(id)
+      const productComponent = this.$refs.deleteModal
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product/${id}`
+      // 打 API
+      this.$http.delete(api)
+        .then((res) => {
+          if (res.data.success) {
+            productComponent.hideModal()
+            this.getProducts()
           }
         })
     }
